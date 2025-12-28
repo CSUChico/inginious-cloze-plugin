347c6429313ad9313b6b41d8da0b4cb31b40f12f
Bryan Dixon PhD <javawolfpack@users.noreply.github.com>
2025-12-28 10:57:34 -0800

    Refactor DisplayableClozeProblem methods

--- src/inginious_cloze_plugin/cloze_problem_frontend.py
+++ src/inginious_cloze_plugin/cloze_problem_frontend.py
@@ -2,8 +2,17 @@ from inginious.frontend.task_problems import DisplayableProblem
 
 class DisplayableClozeProblem(DisplayableProblem):
     @classmethod
-    def get_type_name(cls, language):
+    def get_type(cls) -> str:
+        return "cloze"
+
+    @classmethod
+    def get_type_name(cls, language: str) -> str:
         return "cloze (embedded answers)"
 
-    def show_input(self, template_helper, language, seed):
-        return "<input name='1' />"
+    @classmethod
+    def get_renderer(cls, template_helper):
+        return template_helper.get_renderer(pkg_name=__package__, use_default=False)
+
+    def show_input(self, template_helper, language: str, seed: str) -> str:
+        text = self._problem.get("_display_text", self._problem.get("text", ""))
+        return template_helper.render("cloze_input.html", text=text)
